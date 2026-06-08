5d101bf1ab9d74ddebba6ad65298bf03348dd2a7
Neo Ralph Paez <neo.ralph.b.paez@gmail.com>
2026-06-08 18:51:01 +0800

    6/28/2026 changes

--- React Project/360view/src/panel/ServiceAndMeter.jsx
+++ React Project/360view/src/panel/ServiceAndMeter.jsx
@@ -1,32 +1,32 @@
+import { useState, useEffect } from "react";
 import Table from "../component/Table";
 import style from "../style/ServiceAndMeter.module.css";
 import { accountStatus } from "../lov/translation";
+import getServiceAndMetersApi from "../hooks/getServiceAndMetersApi";
 
 const ServiceAndMeter = () => {
-  const serviceMeterArray = [
-    {
-      serviceType: "000000",
-      serviceStatus: accountStatus["01"],
-      rate: "000000",
-      meterNumber: "00000",
-    },
-    {
-      serviceType: "000000",
-      serviceStatus: accountStatus["02"],
-      rate: "000000",
-      meterNumber: "00000",
-    },
-  ];
+  const serviceAndMeterData = getServiceAndMetersApi();
+  const [serviceAndMeters, setServiceAndMeters] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getServiceAndMetersApi();
+      setServiceAndMeters(data.serviceAndMeters);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={style.main}>
       <Table
         label="Account Services"
         header={["Service Type", "Service Status", "Rate", "Meter Number"]}
-        data={serviceMeterArray.map((i) => {
+        data={serviceAndMeters.map((i) => {
           return (
             <tr className="tableData">
               <td>{i.serviceType}</td>
-              <td>{i.serviceStatus}</td>
+              <td>{accountStatus[i.serviceStatus]}</td>
               <td>{i.rate}</td>
               <td>{i.meterNumber}</td>
             </tr>
